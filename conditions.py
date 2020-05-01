52f25ec2e4f3c863439fc91d62c09cea7b8fd71f
cryptersr <sahilraza78600786@gmail.com>
2020-05-02 01:33:59 +0530

    added variables.py and conditions.py

---
+++ conditions.py
@@ -0,0 +1,8 @@
+x = 0
+
+if x > 0:
+    print("x is positive")
+elif 0 > x:
+    print("x is negative")
+else:
+    print("x is zero")
---
+++ variables.py
@@ -0,0 +1,11 @@
+i = 24
+print(f"i is {i}")
+
+f = 3.5
+print(f"f is {f}")
+
+b = True
+print(f"b is {b}")
+
+n = None
+print(f"n is {n}")
