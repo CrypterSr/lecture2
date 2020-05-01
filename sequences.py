3689b1dd04dd8dc7488d59fecd1264d233a08f64
cryptersr <sahilraza78600786@gmail.com>
2020-05-02 02:00:47 +0530

    added sequences.py

---
+++ sequences.py
@@ -0,0 +1,6 @@
+name = "sahil"
+tuples = ("sa", "ra")
+lists = ["s", "a", "h", "i", "l"]
+dictionary = {"sahil": "raza"}
+
+print(f'name = "{name}"', f"tuples = {tuples}", f"lists = {lists}", f"dictionary = {dictionary}", " " ,  sep = "\n")
